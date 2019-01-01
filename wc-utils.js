f7552b0c226a0ea3e3040f48e8659fbf9ded99d5
Francesco Bedussi <francesco@francescobedussi.it>
2019-01-02 00:22:11 +0100

    change js script

--- wc-utils.js
+++ wc-utils.js
@@ -1,4 +1,4 @@
-import {extendComponent as ec, getAnimationClass as gac, sanitizeString as ss} from 'https://raw.githubusercontent.com/fbedussi/pureact/master/pureact.js';
+import {extendComponent as ec, getAnimationClass as gac, sanitizeString as ss} from 'https://fbedussi.github.io/pureact/pureact.js';
 export const extendComponent = ec;
 export const getAnimationClass = gac;
 export const sanitizeString = ss;
